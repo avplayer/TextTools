8d82328ec4de7d06502f397ff8de2d338c1b6929
Jackarain <jack.wgm@gmail.com>
2020-04-11 02:59:52 +0800

    Very annoying warning

--- TextTools/Encoding/PostSaveProcess.cs
+++ TextTools/Encoding/PostSaveProcess.cs
@@ -43,7 +43,7 @@ namespace TextTools
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [ProvideAutoLoad(UIContextGuids.SolutionExists)]
+    //[ProvideAutoLoad(UIContextGuids.SolutionExists)]
     [ProvideOptionPage(typeof(OptionPageGrid), "TextTools", "PostSave", 0, 0, true)]
     [Guid(PostSaveProcess.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
